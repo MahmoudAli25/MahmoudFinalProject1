33456da9d8f9acab7033ec221af2b63fe63eab3d
razi <razi@mograbi.net>
2022-10-03 13:23:48 +0300

    تطبيق القاآمه

--- app/src/main/java/mahmoud/mahmoudfinalproject/MainActivity.java
+++ app/src/main/java/mahmoud/mahmoudfinalproject/MainActivity.java
@@ -1,14 +1,42 @@
 package mahmoud.mahmoudfinalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(@NonNull Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()==R.id.ITMset)
+        {
+            Intent i=new Intent(MainActivity.this,Settings.class);
+            startActivity(i);
+        }
+        if (item.getItemId()==R.id.ITMhist)
+        {
+            Intent d=new Intent(MainActivity.this,History.class);
+        }
+        return true;
+    }
+
 }
--- app/src/main/java/mahmoud/mahmoudfinalproject/SignUp.java
+++ app/src/main/java/mahmoud/mahmoudfinalproject/SignUp.java
@@ -1,13 +1,19 @@
 package mahmoud.mahmoudfinalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
 
@@ -28,12 +34,6 @@ public class SignUp extends AppCompatActivity {
         BNsave = findViewById(R.id.BNsave);
         BNcan = findViewById(R.id.BNcan);
 
-        BNsave.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                checkAndSave();
-            }
-        });
         BNcan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -45,39 +45,60 @@ public class SignUp extends AppCompatActivity {
 
         private void checkAndSave()//داله تفحص اذا اليميل صحيح وكلمة السر تطابق الاخرى
         {
-            String email=ETmup.getText().toString();
-            String RePasw=ETreup.getText().toString();
-            String passw=ETpup.getText().toString();
-            boolean isok=true;
+            String email = ETmup.getText().toString();
+            String RePasw = ETreup.getText().toString();
+            String passw = ETpup.getText().toString();
+            boolean isok = true;
 
-            if(email.length()*passw.length()*RePasw.length()==0)
-            {
+            if (email.length() * passw.length() * RePasw.length() == 0) {
                 ETmup.setError("One of files are eror");
-                isok=false;
+                isok = false;
             }
 
-            if(passw.equals(RePasw)==false)
-            {
+            if (passw.equals(RePasw) == false) {
                 ETreup.setError("enter the same password");
-                isok=false;
+                isok = false;
             }
-            if(passw.length()==0)
-            {
+            if (passw.length() == 0) {
                 ETpup.setError("enter your password");
-                isok=false;
+                isok = false;
             }
-            if(email.indexOf("@")<=0)
-            {
+            if (email.indexOf("@") <= 0) {
                 ETmup.setError("wrong email");
-                isok=false;
+                isok = false;
             }
-            if(passw.length()<7)
-            {
+            if (passw.length() < 7) {
                 ETpup.setError("password at least 7 character");
-                isok=false;
+                isok = false;
             }
+            if (isok)
+            {
+                FirebaseAuth auth = FirebaseAuth.getInstance();//انشاء حساب من ايميل وسسما
+                auth.createUserWithEmailAndPassword(email, passw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful())
+                        {
+                            Toast.makeText(SignUp.this, "Successful", Toast.LENGTH_SHORT).show();
+                            finish();
+                        }
+                        else
+                        {
+                            Toast.makeText(SignUp.this, "not Successful" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
 
-    }
+                        }
+
+                    }
+                });
+            }
+            BNsave.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view)
+                {
+                    checkAndSave();
+                }
+            });
+        }
 
 
 }
